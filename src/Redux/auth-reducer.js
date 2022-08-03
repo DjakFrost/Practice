9e9b4b1531cdb4f88355c64e1a9a9b25abb87505
DjakFrost <DjakFrost@yandex.ru>
2022-08-03 17:29:40 +0300

    async auth reducer

--- src/Redux/auth-reducer.js
+++ src/Redux/auth-reducer.js
@@ -1,7 +1,7 @@
 import {authAPI} from "../API/api";
 import {stopSubmit} from "redux-form";
 
-const SET_USER_DATA = 'SET_USER_DATA'
+const SET_USER_DATA = 'auth/SET_USER_DATA'
 
 
 let initialState = {
@@ -28,43 +28,33 @@ export const setAuthUserData = (userId, email, login, isAuth) => ({
     data: {userId, email, login, isAuth}
 })
 
-export const getAuthUserData = () => (dispatch) => {
-    return authAPI.headerAuth()
-        .then(data => {
+
+export const getAuthUserData = () => async (dispatch) => {
+   const data = await authAPI.headerAuth()
             if (data.resultCode === 0) {
                 let {id, email, login} = data.data
                 dispatch(setAuthUserData(id, email, login, true))
             }
 
-        })
 }
 
 
-export const login = (email, password, rememberMe) => (dispatch) => {
-
-
-
-
-    authAPI.login(email, password, rememberMe)
-        .then(data => {
+export const login = (email, password, rememberMe) => async(dispatch) => {
+    const data = await authAPI.login(email, password, rememberMe)
             if (data.resultCode === 0) {
                 dispatch(getAuthUserData())
-            }else{
-                let message = data.messages.length>0 ? data.messages[0] : "Wrong login, password"
+            } else {
+                let message = data.messages.length > 0 ? data.messages[0] : "Wrong login, password"
                 dispatch(stopSubmit("login", {_error: message}))
             }
 
-        })
 }
 
-export const logout = () => (dispatch) => {
-    authAPI.logout()
-        .then(data => {
+export const logout = () => async (dispatch) => {
+    const data = await authAPI.logout()
             if (data.resultCode === 0) {
                 dispatch(setAuthUserData(null, null, null, false))
             }
-
-        })
 }
 
 
