373d56cdde830d070b97e3b5d1502decef11167c
DjakFrost <DjakFrost@yandex.ru>
2022-08-03 17:35:23 +0300

    async profile-reducer

--- src/Redux/profile-reducer.js
+++ src/Redux/profile-reducer.js
@@ -51,26 +51,20 @@ export const setStatus = (status) => ({type: SET_STATUS, status})
 export const deletePost = (postId) => ({type: DELETE_POST, postId})
 
 
-export const getUserProfile = (userId)=> (dispatch) => {
-    profileAPI.getProfile(userId)
-        .then(data => {
+export const getUserProfile = (userId)=> async (dispatch) => {
+    const data = await profileAPI.getProfile(userId)
             dispatch(setUserProfile(data))
-
-        })
-}
-export const getStatus = (userId) => (dispatch) =>{
-    profileAPI.getStatus(userId)
-        .then(data =>
-        dispatch(setStatus(data)))
 }
 
-export const updateStatus = (status) => (dispatch) =>{
-    profileAPI.updateStatus(status)
-        .then(data => {
+export const getStatus = (userId) => async (dispatch) =>{
+    const data = await profileAPI.getStatus(userId)
+        dispatch(setStatus(data))
+}
+
+export const updateStatus = (status) => async (dispatch) =>{
+    const data = await profileAPI.updateStatus(status)
             if (data.resultCode === 0) {
                 dispatch(setStatus(status))
-            }
-        })}
-
+            }}
 
 export default profileReducer
